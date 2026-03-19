283c6e699a4a0a040c8ca4b7632fbd83805be420
Orbitaaaaaaaaaal <2026_1801@naesung.hs.kr>
2026-03-19 11:47:31 +0900

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,14 @@
+a = 100 # 정수 (int)
+b = 3.1415 # 실수 (float)
+c = 'Python' # 문자열 (str)
+d = True # T or F (bool)
+print(a, type(a)) # type은 해당 변수의 자료형(datatype)을 보여준다
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+
+colors = ['빨', '주', '노', '초', '파'] # 리스트 (list)(가변형)
+weekday = ('월', '화', '수') # 튜플 (tuple)(불변형)
+students = {'혜민':'131-9898', '서준':'457-4777'} # 딕셔너리(dict)(가변형)
+print(type(colors), type(weekday), type(students))
+
